415eb01d13b928428a7c10253b7f1c711cbbc290
Shaquille <nycshaq@gmail.com>
2022-08-03 11:07:00 -0400

    finished getMostCommonGenre function

--- public/src/books.js
+++ public/src/books.js
@@ -1,43 +1,42 @@
 function findAuthorById(authors, id) {
-  return authors.find(author => author.id === id);
+  return authors.find((author) => author.id === id);
 }
 
 function findBookById(books, id) {
-  return books.find(book => book.id === id);
+  return books.find((book) => book.id === id);
 }
 
 function partitionBooksByBorrowedStatus(books) {
-  let result =[]
-  let booksReturnedArray =[]
-  let booksOutArray =[]
-  
-  books.forEach(book => { 
-    let isBookCheckedOut = book.borrows.some(borrowStatus => !borrowStatus.returned)
-      if(isBookCheckedOut){
-        booksOutArray.push(book)
-      }
-      else{
-        booksReturnedArray.push(book)
-      }
-      
-  })
-  return [booksOutArray, booksReturnedArray]
+  let result = [];
+  let booksReturnedArray = [];
+  let booksOutArray = [];
+
+  books.forEach((book) => {
+    let isBookCheckedOut = book.borrows.some(
+      (borrowStatus) => !borrowStatus.returned
+    );
+    if (isBookCheckedOut) {
+      booksOutArray.push(book);
+    } else {
+      booksReturnedArray.push(book);
     }
-  
+  });
+  return [booksOutArray, booksReturnedArray];
+}
+
 function getBorrowersForBook(book, accounts) {
-  results = []
-// Loop through each account
-accounts.forEach(account => {
-  let currentAccountId = account.id
-  let borrowedBookByAccount = book.borrows.find(borrowedBook => borrowedBook.id === currentAccountId && borrowedBook.returned === true );
+  results = [];
+  accounts.forEach((account) => {
+    let currentAccountId = account.id;
+    let borrowedBookByAccount = book.borrows.find(
+      (borrowedBook) => borrowedBook.id === currentAccountId
+    );
 
-  if(borrowedBookByAccount) {
-    results.push({returned: true, ...account});
-  }
-})
-  
-
-  return results.slice(0, 10); // get 1st 10 items
+    if (borrowedBookByAccount) {
+      results.push({ returned: true, ...account });
+    }
+  });
+  return results.slice(0, 10);
 }
 
 module.exports = {
--- public/src/home.js
+++ public/src/home.js
@@ -1,23 +1,43 @@
 function getTotalBooksCount(books) {
-
-  return books.length
+  return books.length;
 }
 
-
 function getTotalAccountsCount(accounts) {
-  return accounts.length
+  return accounts.length;
 }
 
 function getBooksBorrowedCount(books) {
-  let booksNotReturnedYetCount = 0
-  books.forEach(book => {
-     let booksNotReturned = book.borrows.filter(borrowedBook => !borrowedBook.returned);
-     booksNotReturnedYetCount += booksNotReturned.length;
-  })
+  let booksNotReturnedYetCount = 0;
+  books.forEach((book) => {
+    let booksNotReturned = book.borrows.filter(
+      (borrowedBook) => !borrowedBook.returned
+    );
+    booksNotReturnedYetCount += booksNotReturned.length;
+  });
   return booksNotReturnedYetCount;
 }
 
-function getMostCommonGenres(books) {}
+function getMostCommonGenres(books) {
+  let results = [];
+  let booksByGenreObj = {};
+
+  books.forEach((book) => {
+    let genre = book.genre;
+    if (!booksByGenreObj[genre]) {
+      booksByGenreObj[genre] = 1;
+    } else {
+      booksByGenreObj[genre] += 1;
+    }
+  });
+  for (let keyName in booksByGenreObj) {
+    results.push({
+      name: keyName,
+      count: booksByGenreObj[keyName],
+    });
+    results.sort((a, b) => b.count - a.count);
+  }
+  return results.slice(0, 5);
+}
 
 function getMostPopularBooks(books) {}
 
